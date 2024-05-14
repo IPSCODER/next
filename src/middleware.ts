20fca9a1a2c1fccc48e66d979cc633f359e419af
ipscoder <psawant0015@gmail.com>
2024-05-14 23:27:39 +0530

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: { url: string | URL | undefined; }):any{
+    const user = ""
+
+    if (!user) {
+        return NextResponse.redirect(
+            new URL("/",request.url)
+        )
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher:["/contact","/about"]
+}
